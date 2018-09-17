717528a687ab7e92175e59474f7a7490b2a63ed8
David Doesburg <daviddoesburg@hotmail.com>
2018-09-17 15:16:33 +0200

    changed the audio file

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -1,5 +1,6 @@
 import simpleaudio as sa
 
-wave_obj = sa.WaveObject.from_wave_file("/Users/Daviddoesburg/Desktop/Puredata_wav_directory/Fight_pad.wav")
+wave_obj =
+sa.WaveObject.from_wave_file("/Users/Daviddoesburg/Desktop/Puredata_wav_directory/Atmo_Conga_01.wav")
 play_obj = wave_obj.play()
 play_obj.wait_done()
